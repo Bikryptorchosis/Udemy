3ed9daa54984280e74599e8ebd6df438ff75c859
Piotr <frinn.wolf@gmail.com>
2018-11-04 22:32:11 +0100

    Base commit

--- enemy.py
+++ enemy.py
@@ -4,6 +4,7 @@ class Enemy:
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
@@ -12,6 +13,11 @@ class Enemy:
             print("I took {} damage and have {} left".format(damage, self.hit_points))
         else:
             self.lives -= 1
+            if self.lives > 0:
+                print("{0.name} lost a life".format(self))
+            else:
+                print("{0.name} is dead".format(self))
+                self.alive = False
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Hitpoints: {0.hit_points}".format(self)
@@ -26,4 +32,7 @@ class Troll(Enemy):
         print("Me {0.name}. {0.name} stomp you".format(self))
 
 
+class Vampire(Enemy):
 
+    def __init__(self, name):
+        super().__init__(name=name, lives=3, hit_points=12)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from enemy import Troll, Enemy
+from enemy import Troll, Enemy, Vampire
 
 ugly_troll = Troll("Purg")
 print("Ugly troll - {}".format(ugly_troll))
@@ -12,3 +12,16 @@ print(brother_troll)
 ugly_troll.grunt()
 another_troll.grunt()
 brother_troll.grunt()
+
+
+ugly_troll.take_damage(10)
+print(ugly_troll)
+
+dracula = Vampire("Alucard")
+print(dracula)
+dracula.take_damage(10)
+print(dracula)
+
+while dracula.alive:
+    dracula.take_damage(1)
+    print(dracula)

1a205ef3ad4584c9bee007120e7533adcfea140e
Piotr <frinn.wolf@gmail.com>
2018-11-09 20:22:17 +0100

    Base commit

--- tkinterstuff/screen.py
+++ tkinterstuff/screen.py
@@ -31,7 +31,7 @@ fileList.config(border=2, relief='sunken')
 for zone in os.listdir('/Windows/System32'):
     fileList.insert(tkinter.END, zone)
 
-listScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=fileList.yview())
+listScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=fileList.yview)
 listScroll.grid(row=1, column=1, sticky='nsw', rowspan=2)
 fileList['yscrollcommand'] = listScroll.set
 
@@ -48,6 +48,44 @@ radio3 = tkinter.Radiobutton(optionFrame, text='Timestamp', value=3, variable=rb
 radio1.grid(row=0, column=0, sticky='w')
 radio2.grid(row=1, column=0, sticky='w')
 radio3.grid(row=2, column=0, sticky='w')
+
+# result display shiz
+resultLabel = tkinter.Label(mainWindow, text='Result')
+resultLabel.grid(row=2, column=2, sticky='nw')
+result = tkinter.Entry(mainWindow)
+result.grid(row=2, column=2, sticky='sw')
+
+# time spinners
+timeFrame = tkinter.LabelFrame(mainWindow, text='Time')
+timeFrame.grid(row=3, column=0, sticky='new')
+hourSpinner = tkinter.Spinbox(timeFrame, width=2, values=tuple(range(0, 24)))
+minuteSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+secondSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+
+hourSpinner.grid(row=0, column=0)
+tkinter.Label(timeFrame, text=':').grid(row=0, column=1)
+minuteSpinner.grid(row=0, column=2)
+tkinter.Label(timeFrame, text=':').grid(row=0, column=3)
+secondSpinner.grid(row=0, column=4)
+timeFrame['padx'] = 36
+
+# date spinnorz
+dateFrame = tkinter.Frame(mainWindow)
+dateFrame.grid(row=4, column=0, sticky='new')
+
+dayLabel = tkinter.Label(dateFrame, text='Day')
+monthLabel = tkinter.Label(dateFrame, text='Month')
+yearLabel = tkinter.Label(dateFrame, text='Year')
+dayLabel.grid(row=0, column=0, sticky='w')
+monthLabel.grid(row=0, column=1, sticky='w')
+yearLabel.grid(row=0, column=2, sticky='w')
+
+daySpin = tkinter.Spinbox(dateFrame, width=5, from_=1, to=31)
+monthSpin = tkinter.Spinbox(dateFrame, width=5, values=('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+yearSpin = tkinter.Spinbox(dateFrame, width=5, from_=2000, to=2099)
+daySpin.grid(row=1, column=0)
+monthSpin.grid(row=1, column=1)
+yearSpin.grid(row=1, column=2)
 
 mainWindow.mainloop()
 

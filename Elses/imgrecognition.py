306d7ec801d1657a17a5cba6485681f976c5b166
Piotr <frinn.wolf@gmail.com>
2020-12-30 00:44:09 +0100

    gyg

--- Elses/imgrecognition.py
+++ Elses/imgrecognition.py
@@ -1,6 +1,9 @@
 import cv2
+import numpy as np
 import pytesseract
 import glob
+import tqdm
+
 try:
     from PIL import Image
 except ImportError:
@@ -9,16 +12,32 @@ except ImportError:
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 
+def ocr_core(img):
+    text = pytesseract.image_to_string(img)
+    return text
+
+
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+def remove_noise(image):
+    return cv2.medianBlur(image, 5)
+
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
 kupons = [f for f in glob.glob("*.jpg")]
 print(kupons)
 
-for kupon in kupons:
+for kupon in tqdm.tqdm(kupons):
     print(kupon)
-    img = cv2.imread("{}".format(kupon))
-    # img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_rgb = Image.frombytes('RGB', img.shape[:2], img, 'raw', 'BGR', 0, 0)
-    text = pytesseract.image_to_string(img_rgb)
-    print(text)
-    # text = pytesseract.image_to_string(img, config='--psm 7 -c tessedit_char_whitelist=abcdefghijkLmnoprstuwxyzq0123456789')
-    # print(pytesseract.image_to_string(Image.open(img_rgb)))
-
+    img = cv2.imread('{}'.format(kupon))
+    img = get_grayscale(img)
+    img = thresholding(img)
+    img = remove_noise(img)
+    print(ocr_core(img))
+    with open("kody.txt", "a") as file_object:
+        file_object.write(ocr_core(img))
